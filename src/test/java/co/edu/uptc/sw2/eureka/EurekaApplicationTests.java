9fc9de5dbdce4307946dbfb996c5cb34de80a51e
Angel David Torres Castellanos <angeldavidtorresadtc@gmail.com>
2026-03-27 22:06:53 -0500

    feat: commit inicial con la estructura del servidor Eureka

---
+++ src/test/java/co/edu/uptc/sw2/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.sw2.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
